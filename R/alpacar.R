6ba81acc417343b13ed6f0d5f072ddaddc038a7d
Andrew Collier <andrew@fathomdata.dev>
2024-09-26 04:51:13 +0100

    fix: Consolidate .onLoad and print version at start

--- R/alpacar.R
+++ R/alpacar.R
@@ -5,15 +5,6 @@
 #' @import lubridate
 NULL
 
-.onLoad <- function(libname, pkgname) {
-  # nolint start
-  # nocov start
-  options(digits.secs = 3)
-  log_layout(layout_glue_generator('{stringr::str_pad(level, 7, side = "right")} [{time}] {msg}'))
-  # nocov end
-  # nolint end
-}
-
 globalVariables(
   c(
     "GET",
--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,13 @@
 .onLoad <- function(libname, pkgname) {
+  # nolint start
+  # nocov start
+  options(digits.secs = 3)
+  log_layout(layout_glue_generator('{stringr::str_pad(level, 7, side = "right")} [{time}] {msg}'))
+
+  message("alpacar v", as.character(packageVersion("alpacar")))
   message("Selecting paper trading API by default.")
+
   base_url(BASE_URL_PAPER_TRADING)
+  # nocov end
+  # nolint end
 }
