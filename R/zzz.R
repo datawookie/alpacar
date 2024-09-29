faf8b29d84bba720bed36a57e2c2d60bf7c88b81
Andrew Collier <andrew@fathomdata.dev>
2024-09-29 15:37:42 +0100

    feat: Fix precision of log timing

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,6 @@
 .onLoad <- function(libname, pkgname) {
   # nolint start
   # nocov start
-  options(digits.secs = 3)
   log_layout(layout_glue_generator('{stringr::str_pad(level, 7, side = "right")} [{time}] {msg}'))
 
   message("alpacar v", as.character(packageVersion("alpacar")))
