ec4ef253198c6dcbc736a15ce71b592ab261dcbe
Andrew Collier <andrew@fathomdata.dev>
2024-09-29 16:00:07 +0100

    fix: Convert from string to numeric

--- R/account.R
+++ R/account.R
@@ -8,5 +8,12 @@
 #' account()
 #' }
 account <- function() {
-  GET(base_url(), "account")
+  details <- GET(base_url(), "account")
+
+  # TODO: Helper function to do this in loop (other fields required too!).
+  details$cash <- as.numeric(details$cash)
+  details$portfolio_value <- as.numeric(details$portfolio_value)
+  details$equity <- as.numeric(details$equity)
+
+  details
 }
